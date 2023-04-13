eba5a394e5c9e878374ecdc9b9dcb8214d53046c
Abhi Pandey <97884726+abhipandey1402@users.noreply.github.com>
2023-04-13 22:38:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Person(name, age) {
 	this.age=age;
 }
 Person.prototype.greet=function(){
-	return "Hello, my name is " + this.name +", I am "+this.age+" years old.";
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 };
 // var p=new Person('areeb',23);
 // console.log(p);
@@ -17,8 +17,10 @@ function Employee(name, age, jobTitle) {
 }
 
 Employee.prototype=Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
 Employee.prototype.jobGreet=function(){
-	return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+	console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
 
 // Do not change code below this line
