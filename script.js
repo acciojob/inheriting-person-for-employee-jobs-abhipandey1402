754027739506dd5bf7919627ef3e4b78deda6eef
Abhi Pandey <97884726+abhipandey1402@users.noreply.github.com>
2023-04-13 22:27:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
 // complete this js code
+// complete this js code
 function Person(name, age) {
-	this.name = name;
-	this.age = age;
-
-	greet(){
-		console.log(`Hello,  my name is ${this.name} , I am ${this.age} years old`);
-	}
+	this.name=name;
+	this.age=age;
 }
+Person.prototype.greet=function(){
+	return "Hello, my name is " + this.name +", I am "+this.age+" years old.";
+};
+// var p=new Person('areeb',23);
+// console.log(p);
+// console.log(Object.getPrototypeOf(p));
 
 function Employee(name, age, jobTitle) {
-	super(name, age);
-	this.jobTitle = jobTitle;
-
-	jobGreet(){
-		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`);
-	}
+	Person.call(this,name,age);
+	this.jobTitle=jobTitle;
 }
+
+Employee.prototype=Object.create(Person.prototype);
+Employee.prototype.jobGreet=function(){
+	return `Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`
+};
 
 // Do not change code below this line
 window.Person = Person;
